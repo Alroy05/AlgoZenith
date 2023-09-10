6ebd4d5e47899bb8c26cc88e6029b37fa5627998
Rudalph <gonsalvesrudalph@gmail.com>
2023-09-10 14:14:33 +0530

    Database Connection

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,25 +1,59 @@
 import React, { useState } from 'react';
 import './Register.css'; 
 import Fade from 'react-reveal/Fade';
+import {app} from '../firebase';
+import { collection, addDoc, getDocs } from "firebase/firestore";
+import {db} from '../firebase';
+import { Link } from 'react-router-dom';
+
 
 function Register() {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
     phoneNumber: '',
-    subject: '',
-    message: '',
+    rollNumber: '',
+    year: '',
+    branch:''
   });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
+
+  // const handleChange = (e) => {
+  //   const { name, value } = e.target;
+  //   setFormData({ ...formData, [name]: value });
+  // };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(formData);
-  };
+
+        if(formData.name=='' || formData.email=='' || formData.phoneNumber=='' || formData.rollNumber=='' || formData.year=='' || formData.branch=='')
+        {
+          alert("Some fields are incomplete");
+        }
+        else{
+          try{
+            const docRef = addDoc(collection(db,"collections"),
+            {
+              name:formData.name,
+              email:formData.email,
+              phoneNumber:formData.phoneNumber,
+              rollNumber:formData.rollNumber,
+              year:formData.year,
+              branch:formData.branch,
+            
+            });
+            console.log("successfully data stored")
+            alert("Registration successful")
+        } catch(e)
+        {
+        console.log("Couldnot store data")
+        }
+        };
+       }
+          
+
+    
 
   return (
     <Fade bottom>
@@ -31,7 +65,7 @@ function Register() {
             name="name"
             placeholder="Name"
             value={formData.name}
-            onChange={handleChange}
+            onChange={(event)=>setFormData((prev) => ({...prev, name: event.target.value}))}
             className="i-field"
           />
           <div className="inline-fields">
@@ -40,7 +74,7 @@ function Register() {
               name="email"
               placeholder="Email"
               value={formData.email}
-              onChange={handleChange}
+              onChange={(event)=>setFormData((prev) => ({...prev, email: event.target.value}))}
               className="i-field"
             />
             <input
@@ -48,7 +82,7 @@ function Register() {
               name="phoneNumber"
               placeholder="Phone Number"
               value={formData.phoneNumber}
-              onChange={handleChange}
+              onChange={(event)=>setFormData((prev) => ({...prev, phoneNumber: event.target.value}))}
               className="i-field"
             />
           </div>
@@ -57,10 +91,11 @@ function Register() {
               type="number"
               name="roll-no"
               placeholder="Roll No.(4-digit)"
+              onChange={(event)=>setFormData((prev) => ({...prev, rollNumber: event.target.value}))}
               className="i-field"
             />
             
-            <select name="year" id="year" className='s-field'>
+            <select name="year" id="year" className='s-field' onChange={(event)=>setFormData((prev) => ({...prev, year: event.target.value}))}>
               <option disabled selected value="">Select a year</option>
               <option value="FE">FE</option>
               <option value="SE">SE</option>
@@ -68,7 +103,7 @@ function Register() {
               <option value="BE">BE</option>
             </select> 
             
-            <select name="branch" id="branch" className='s-field'>
+            <select name="branch" id="branch" className='s-field' onChange={(event)=>setFormData((prev) => ({...prev, branch: event.target.value}))}>
               <option disabled selected value="">Select a Branch</option>
               <option value="ComputerA">Computer A</option>
               <option value="ComputerB">Computer B</option>
@@ -78,7 +113,7 @@ function Register() {
             </select>
           </div>
           
-          <button type="submit" className="sub-button">Submit</button>
+        <button type="submit" className="sub-button">Submit</button>
         </form>
       </div>
     </Fade>
---
+++ src/firebase.js
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import {getStorage} from 'firebase/storage';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDVL_a-SMaIusmyATXRUeDSHPdPrvlntKg",
+  authDomain: "algozenith-ad2c2.firebaseapp.com",
+  projectId: "algozenith-ad2c2",
+  storageBucket: "algozenith-ad2c2.appspot.com",
+  messagingSenderId: "896503105598",
+  appId: "1:896503105598:web:3d36333449b1547a45e96f",
+  measurementId: "G-ZXKLQY0RB6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export {app };
+// export default db;
+export const storage = getStorage(app);
+export const db = getFirestore(app);
